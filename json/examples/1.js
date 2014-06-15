ae7c456d24558b02e822667771bb2d4edc144ff9
David Teirney <david.teirney@orionhealth.com>
2014-06-15 22:28:32 +1200

    Add example json response from the MAXX jsonp API.

---
+++ json/examples/1.js
@@ -0,0 +1,42 @@
+callback({
+	"Error": null,
+	"Extensions": [{
+		"Priority": "high",
+		"Text": "<<< Buses replace trains on the Western Line after 8.30pm, Sun to Thurs until further notice. www.AT.govt.nz>>>"
+	}],
+	"Movements": [{
+		"ActualArrivalTime": "\/Date(1402216320000)\/",
+		"ActualDepartureTime": "\/Date(1402216320000)\/",
+		"ArrivalBoardingActivity": "alighting",
+		"ArrivalPlatformName": "2",
+		"ArrivalStatus": "noReport",
+		"DepartureBoardingActivity": "boarding",
+		"DeparturePlatformName": "2",
+		"DestinationDisplay": "SWANSON",
+		"ExpectedArrivalTime": "\/Date(1402216265000)\/",
+		"ExpectedDepartureTime": "\/Date(1402216360000)\/",
+		"InCongestion": false,
+		"Monitored": true,
+		"Route": "WEST",
+		"Stop": "9313",
+		"TimeStamp": "\/Date(1402212774744)\/",
+		"VehicleJourneyName": null
+	}, {
+		"ActualArrivalTime": "\/Date(1402213440000)\/",
+		"ActualDepartureTime": "\/Date(1402213440000)\/",
+		"ArrivalBoardingActivity": "alighting",
+		"ArrivalPlatformName": "1",
+		"ArrivalStatus": "noReport",
+		"DepartureBoardingActivity": "boarding",
+		"DeparturePlatformName": "1",
+		"DestinationDisplay": "BRITOMART",
+		"ExpectedArrivalTime": "\/Date(1402213484000)\/",
+		"ExpectedDepartureTime": "\/Date(1402213587000)\/",
+		"InCongestion": false,
+		"Monitored": true,
+		"Route": "WEST",
+		"Stop": "9312",
+		"TimeStamp": "\/Date(1402212774744)\/",
+		"VehicleJourneyName": null
+	}]
+});
